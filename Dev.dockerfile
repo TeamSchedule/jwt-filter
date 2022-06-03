46e2cc83f8887e9b587f871af9b1442c2e215596
Sergey <sergey.rubnikovich@gmail.com>
2022-06-03 20:39:33 +0700

    Use Gin

--- Dev.dockerfile
+++ Dev.dockerfile
@@ -1,11 +1,12 @@
-ARG GO_VERSION=1.18
-FROM golang:${GO_VERSION}-alpine
-
+ARG GO_VERSION=1.18.1
+FROM golang:${GO_VERSION}-alpine as jwt-filter-deps
 WORKDIR /jwt-filter
+COPY . .
+RUN go build -buildvcs=false -o jwt-filter .
 
-COPY ./go.mod .
-COPY ./main.go .
 
-RUN go get ./...
-
-ENTRYPOINT go run main.go
+FROM jwt-filter-deps as jwt-filter-runner
+WORKDIR /jwt-filter
+COPY --from=jwt-filter-deps /jwt-filter/jwt-filter .
+RUN chmod 755 ./jwt-filter
+ENTRYPOINT ./jwt-filter
--- main.go
+++ main.go
@@ -1,69 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"github.com/dgrijalva/jwt-go"
+	"jwt-filter/server"
+	"jwt-filter/server/conf"
 	"log"
-	"net/http"
-	"os"
-	"strings"
 )
 
-var JwtSecretKey = os.Getenv("JWT_SECRET_KEY")
-var JwtFilterPort = os.Getenv("JWT_FILTER_PORT")
-
-func validateAuthorizationToken(w http.ResponseWriter, r *http.Request) {
-	var statusCode = http.StatusUnauthorized
-	var msg = ""
-
-	AuthorizationHeader := r.Header.Get("Authorization")
-	if !strings.HasPrefix(AuthorizationHeader, "Bearer ") {
-		w.WriteHeader(statusCode)
-		return
-	}
-
-	tokenString := strings.Split(AuthorizationHeader, " ")[1]
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return []byte(JwtSecretKey), nil
-	})
-
-	if token.Valid {
-		statusCode = http.StatusOK
-		msg = "Good token"
-	} else if ve, ok := err.(*jwt.ValidationError); ok {
-		if ve.Errors&jwt.ValidationErrorMalformed != 0 {
-			msg = "That's not even a token"
-		} else if ve.Errors&(jwt.ValidationErrorExpired|jwt.ValidationErrorNotValidYet) != 0 {
-			msg = "Token expired"
-		} else {
-			msg = "Couldn't handle this token:"
-		}
-	} else {
-		msg = "Couldn't handle this token:"
-	}
-
-	w.WriteHeader(statusCode)
-	fmt.Fprintf(w, msg)
-}
-
 func main() {
-	if JwtSecretKey == "" {
-		log.Fatal("environment variable `JWT_SECRET_KEY` must be specified and must be non empty value")
-	}
+	r := server.SetupServer()
 
-	if JwtFilterPort == "" {
-		log.Fatal("environment variable `JWT_FILTER_PORT` must be specified and must be non empty value")
-	}
-
-	log.Println("JWT-FILTER SERVER START SUCCESSFULLY")
-
-	// Устанавливаем роутер
-	http.HandleFunc("/", validateAuthorizationToken)
-
-	// устанавливаем порт веб-сервера
-	err := http.ListenAndServe(":"+JwtFilterPort, nil)
-
+	err := r.Run(":" + conf.JwtFilterPort)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Fatalln("JWT_FILTER service crashed!")
+		return
 	}
 }
